a9abd851b1b434356e43043dceff4130c15dd3bb
Hrushikesh Patel <hrushikesh592002@gmail.com>
2022-05-10 22:00:12 +0530

    Deploy gh-pages

--- _next/static/WlRjkWjhtdtIfhrtnit_A/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/fd4f66849d6d161e.css","static/chunks/pages/index-079cb602e26f087c.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ _next/static/e8AiKCIpgQnObgnvKcVl0/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/eb971c539c4bcd5f.css","static/chunks/pages/index-17db4d7a70500d9e.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
